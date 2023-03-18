4028c1dd5bab23327694b1f68c814298ebbfe3fa
1A291109A6 <128209125+1A291109A6@users.noreply.github.com>
2023-03-18 22:44:33 +0900

    Update project_file.js

--- js/project_file.js
+++ js/project_file.js
@@ -1,1 +1,19 @@
-alert('JavaScriptが読み込まれました');
+function generateNormalDistribution(mean, variance) {
+  // 0から1の間の2つの乱数を生成する
+  var u1 = Math.random();
+  var u2 = Math.random();
+  
+  // Box-Muller法を使用して正規分布に従う乱数を生成する
+  var z1 = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+  var z2 = Math.sqrt(-2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
+  
+  // 平均と分散を考慮して値を調整する
+  var x = mean + Math.sqrt(variance) * z1;
+  
+  return x;
+}
+
+function generateRandomNumber(mean, variance) {
+			var randomNumber = generateNormalDistribution(mean, variance); 
+			document.getElementById("result").innerHTML = "数値: " + randomNumber;
+		}
